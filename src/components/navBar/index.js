a2130b7bda07a6edef327195b474dc227701ca77
Juan Gremes <gremjua@gmail.com>
2021-01-31 21:56:39 -0300

    add product categories

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -8,14 +8,14 @@ import CartWidget from './CartWidget';
 import Button from '@material-ui/core/Button';
 
 const NavBar = () => {
-    const sections = ['Home', 'Productos', 'Contactanos', 'Sobre nosotros'];
-    const spacing = '6';
+    const sections = ['Productos para exterior', 'Productos para interior', 'Productos a medida'];
+    const spacing = 3;
     return (
         <div>
             <AppBar position="static">
                 <Toolbar>
                     <NatureIcon fontSize="large"/>
-                    <Typography variant="h6" align="center">
+                    <Typography variant="h6">
                         Toio Maderas
                     </Typography>
                     <Grid container justify="center" spacing={spacing} >
