af313ecba6755471f8422de529a70a58436da5a3
Juan Gremes <gremjua@gmail.com>
2021-03-01 19:20:40 -0300

    fix CartWidget component router link

--- src/components/navBar/CartWidget.js
+++ src/components/navBar/CartWidget.js
@@ -1,9 +1,15 @@
 import { IconButton } from '@material-ui/core';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import { Link as RouterLink } from 'react-router-dom';
 
 const CartWidget = () => {
     return (
-        <IconButton color="inherit" href="/cart" aria-label="view shopping cart">
+        <IconButton
+            component={RouterLink}
+            to="/cart"
+            color="inherit"
+            aria-label="view shopping cart"
+        >
             <ShoppingCart fontSize="large" />
         </IconButton>
     );
