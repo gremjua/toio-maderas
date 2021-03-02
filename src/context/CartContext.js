c39bad1c2cea766312a80b5ce4123acd4204d780
Juan Gremes <gremjua@gmail.com>
2021-03-01 22:08:03 -0300

    add functions to CartContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -14,6 +14,16 @@ export const CartProvider = ({ children }) => {
             { item, quantity: actualQuantity },
         ]);
     };
+    const subtractItem = (item, quantity) => {
+        let itemQuantity = cart.find((el) => el.item.id === item.id)?.quantity || 0;
+        itemQuantity -= quantity;
+        itemQuantity <= 0
+            ? removeItem(item.id)
+            : setCart([
+                  ...cart.filter((el) => el.item.id !== item.id),
+                  { item, quantity: itemQuantity },
+              ]);
+    };
     const removeItem = (itemId) => {
         setCart([...cart.filter((el) => el.item.id !== parseInt(itemId))]);
     };
@@ -21,8 +31,21 @@ export const CartProvider = ({ children }) => {
     const isInCart = (itemId) => {
         return cart.findIndex((el) => el.item.id === parseInt(itemId)) !== -1;
     };
+    const getTotals = () =>
+        cart.reduce(
+            (accumulator, current) => {
+                return {
+                    totalQuantity: accumulator.totalQuantity + current.quantity,
+                    totalPrice: accumulator.totalPrice + current.quantity * current.item.price,
+                };
+            },
+            { totalQuantity: 0, totalPrice: 0 },
+        );
+
     return (
-        <CartContext.Provider value={{ cart, addItem, removeItem, clear, isInCart }}>
+        <CartContext.Provider
+            value={{ cart, addItem, subtractItem, removeItem, clear, isInCart, getTotals }}
+        >
             {children}
         </CartContext.Provider>
     );
